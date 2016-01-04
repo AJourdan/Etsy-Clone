11420763db9b0c7154e4fe317270b96240684123
Alon Jourdan <alon.jourdan@gmail.com>
2016-01-03 21:53:28 -0500

    Cleaning up repeated ccode

--- www/js/controllers/photo.js
+++ www/js/controllers/photo.js
@@ -1,21 +1,27 @@
 'use strict';
 
 app.controller('PhotoCtrl', function($scope, $cordovaCamera, $rootScope, $state) {
+
+	var options = {
+		quality: 75,
+		destinationType: Camera.DestinationType.DATA_URL,
+		allowEdit: true,
+		encodingType: Camera.EncodingType.JPEG,
+		popoverOptions: CameraPopoverOptions,
+		targetWidth: 500,
+		targetHeight: 500,
+		saveToPhotoAlbum: false
+	};
+
+
+
+
 	$scope.takePhoto = function() {
 		console.log('takePhoto was clicked');
 		// Camera Plugin
+		options.sourceType = Camera.PictureSourceType.CAMERA,		
 
-		var options = {
-			quality: 75,
-			destinationType: Camera.DestinationType.DATA_URL,
-			sourceTrype: Camera.PictureSourceType.CAMERA,
-			allowEdit: true,
-			encodingType: Camera.EncodingType.JPEG,
-			popoverOptions: CameraPopoverOptions,
-			targetWidth: 500,
-			targetHeight: 500,
-			saveToPhotoAlbum: false
-		};
+
 
 		$cordovaCamera.getPicture(options).then(function(photo){
 			$rootScope.imgURI = "data:image/jpeg;base64," +photo;
@@ -25,22 +31,11 @@ app.controller('PhotoCtrl', function($scope, $cordovaCamera, $rootScope, $state)
 
 	$scope.choosePhoto = function() {
 		console.log('will link up to PhotoLibrary');
-				var options = {
-			quality: 75,
-			destinationType: Camera.DestinationType.DATA_URL,
-			sourceTrype: Camera.PictureSourceType.PHOTOLIBRARY,
-			allowEdit: true,
-			encodingType: Camera.EncodingType.JPEG,
-			popoverOptions: CameraPopoverOptions,
-			targetWidth: 500,
-			targetHeight: 500,
-			saveToPhotoAlbum: false
-		};
+		options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY,
 
 		$cordovaCamera.getPicture(options).then(function(photo){
 			$rootScope.imgURI = "data:image/jpeg;base64," +photo;
 			$state.go('tab.photo-detail');
 		})
-	}
 	}
 });
